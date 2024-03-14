65fcea826c58e1f7a6c096c7c6c8e92548fa82ad
李欣欣 <lixinxin@rtmap.com>
2024-03-14 11:01:27 +0800

    fix

--- llama_index/02_starter.py
+++ llama_index/02_starter.py
@@ -2,29 +2,13 @@ from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings, 
 from llama_index.llms.ollama import Ollama
 
 if __name__ == "__main__":
-
-    # # doc
-    # documents = SimpleDirectoryReader("data").load_data()
-    #
-    # # llm
-    # Settings.llm = Ollama(model="gemma:2b")
-    # Settings.llm.base_url = "http://1.92.64.112:11434"
-    #
-    # index = VectorStoreIndex.from_documents(
-    #     documents,
-    # )
-    #
-    # query_engine = index.as_query_engine()
-    # response = query_engine.query("introduce me Paul Graham")
-    # print(response)
-
     # 定义你的LLM
     llm = Ollama(model="gemma:2b")
     llm.base_url = "http://1.92.64.112:11434"
 
     # 定义你的服务上下文
     service_context = ServiceContext.from_defaults(
-        llm=llm, embed_model="embed_model/bge-small-en-v1.5"
+        llm=llm, embed_model="local:D:/C 代码/lllama/llamaindex_learn/llama_index/embed_model/bge-small-en-v1.5"
     )
 
     # 加载你的数据

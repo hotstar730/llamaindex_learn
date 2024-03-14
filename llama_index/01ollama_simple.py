db5545f27b1558c328534c7c0a1db44269f3c590
李欣欣 <lixinxin@rtmap.com>
2024-03-14 10:26:08 +0800

    添加访问远端ollama示例代码

---
+++ llama_index/01ollama_simple.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# encoding=utf-8
+
+"""
+    项目名称:  llamaindex_learn
+    文件名称： 01ollama_simple.py.py
+    功能描述： ..
+    创建者：   lixinxin
+    创建日期： 2024/3/14 10:19
+"""
+
+from llama_index.llms.ollama import Ollama
+
+if __name__ == "__main__":
+    llm = Ollama(model="gemma:2b")
+    llm.base_url = "http://1.92.64.112:11434"
+    response = llm.complete("写一篇作文，题目是：我的妈妈?")
+    print(response)

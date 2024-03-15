1989fe565e0419e0e913f2033f3439378a6c2644
李欣欣 <lixinxin@rtmap.com>
2024-03-15 13:27:42 +0800

    fix 添加时间搓打印

--- llama_index_example/4_flask_demo1.py
+++ llama_index_example/4_flask_demo1.py
@@ -1,32 +1,17 @@
-# Import modules
-import os
-
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
-from llama_index.core import SimpleDirectoryReader, ServiceContext, SummaryIndex
+from llama_index.core.base.llms.types import ChatMessage
 from llama_index.llms.ollama import Ollama
 
-
 # 定义你的LLM
-llm = Ollama(model="pxlksr/defog_sqlcoder-7b-2:Q8")
-llm.temperature = 0.2
+llm = Ollama(model="qwen:7b-chat")
+llm.temperature = 0.7
 llm.base_url = "http://1.92.64.112:11434"
-
-# 定义你的服务上下文
-model_dir = os.path.abspath('embed_model/bge-small-en-v1.5')
-service_context = ServiceContext.from_defaults(
-    llm=llm, embed_model="local:" + model_dir
-)
-
-# 加载你的数据
-documents = SimpleDirectoryReader("data").load_data()
-index = SummaryIndex.from_documents(documents, service_context=service_context)
 
 # 启动 Flask server
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
-
 
 @app.route('/')
 def hello_world():
@@ -37,12 +22,19 @@ def hello_world():
 def chat():
     query = request.args.get('query') if request.method == 'GET' else request.form.get('query')
     if query is not None:
-        query_engine = index.as_query_engine()
-        response = query_engine.query(query)
+        messages = [
+            # ChatMessage(
+            #     role="system", content="You are a pirate with a colorful personality"
+            # ),
+            ChatMessage(role="user", content=query),
+        ]
+        for message in messages:
+            print(message.role.value, message.content)
+        response = llm.chat(messages)
+        print(response)
         return jsonify({"response": str(response)})
     else:
         return jsonify({"error": "query field is missing"}), 400
-
 
 if __name__ == '__main__':
     app.run()

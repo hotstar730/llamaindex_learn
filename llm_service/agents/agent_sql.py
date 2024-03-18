5ae3bdf22ac61f4ba56444a52821d1244b567c36
李欣欣 <lixinxin@rtmap.com>
2024-03-18 08:52:46 +0800

    清理无效代码

--- llm_service/agents/agent_sql.py
+++ llm_service/agents/agent_sql.py
@@ -7,12 +7,12 @@
     功能描述： ..
     创建者：   lixinxin
     创建日期： 2024/3/14 13:47
+    需要使用mysql数据进行测试
 """
 
 import os
 from typing import List
-from sqlalchemy import insert
-from IPython.display import Markdown, display
+
 from llama_index.core import SQLDatabase
 from llama_index.core.indices.struct_store import NLSQLTableQueryEngine
 from llama_index.core.llms import ChatMessage
@@ -20,10 +20,7 @@ from llama_index.llms.ollama import Ollama
 from sqlalchemy import (
     create_engine,
     MetaData,
-    Table,
-    Column,
-    String,
-    Integer, text,
+    text,
 )
 
 from llm_service.agents.agent_base import AgentBase
@@ -31,9 +28,33 @@ from llm_service.util.excel_util import ExcelUtil
 
 
 class AgentSql(AgentBase):
-    def __init__(self) -> None:
-        print("init")
 
+    def __init__(self) -> None:
+        filepath = './data/excel/test.xlsx'
+        print("init")
+        filepath = './data/excel/test.xlsx'
+        table_name = os.path.splitext(os.path.basename(filepath))[0]
+
+        self.meta_data = MetaData()
+        self.engine = create_engine("mysql://root:@localhost/testdb", pool_recycle=3306, echo=True)
+        self.meta_data.create_all(self.engine)
+        self.database = SQLDatabase(self.engine, include_tables=[table_name])
+
+        # 读取成功
+        self.connect = self.database.engine.connect()
+        rows = self.connect.execute(text("SELECT vin from test"))
+        for row in rows:
+            print(row)
+
+        # 定义你的LLM
+        self.llm = Ollama(model="pxlksr/defog_sqlcoder-7b-2:Q8")
+        self.llm.temperature = 0.2
+        self.llm.base_url = "http://1.92.64.112:11434"
+
+        # 查询引擎
+        self.query_engine = NLSQLTableQueryEngine(
+            sql_database=self.database, tables=[table_name], llm=self.llm
+        )
 
     def _read_excel(self, filename):
         data_rows = []
@@ -56,53 +77,15 @@ class AgentSql(AgentBase):
         return data_rows
 
     def chat(self, messages: List[ChatMessage]) -> ChatMessage:
-        filepath = './data/excel/test.xlsx'
-        table_name = os.path.splitext(os.path.basename(filepath))[0]
-
-        metadata_obj = MetaData()
-        city_stats_table = Table(
-            table_name,
-            metadata_obj,
-            Column("vin", String(16), primary_key=True),
-            Column("库存类型", String(16), nullable=False),
-            Column("是否上牌", String(16), nullable=False),
-            Column("经销商编码", String(16), nullable=False),
-            Column("经销商名称", String(16), nullable=False),
-            Column("送达方", String(16)),
-            Column("终端离线时长", Integer),
-            Column("车联网实销状态", String(16), nullable=False),
-            Column("TSP库存名称", String(16)),
-            Column("DMS库存类型", String(16)),
-            Column("SAP库存类型", String(16)),
-            Column("车联网库存类型与（DMS库存类型是否匹配或者SAP库存类型）", String(16), nullable=False),
-            Column("不一致原因", String(16)),
-            Column("入中心库时间", String(16)),
-            Column("库龄", String(16)),
-        )
-        engine = create_engine("sqlite:///:memory:")
-        metadata_obj.create_all(engine)
-        sql_database = SQLDatabase(engine, include_tables=[table_name])
-
-        # 插入数据
-        rows = self._read_excel(filepath)
+        # self.metadata_obj.create_all(self.sql_database.engine)
+        rows = self.connect.execute(text("SELECT vin from test"))
         for row in rows:
-            stmt = insert(city_stats_table).values(**row)
-            with engine.begin() as connection:
-                cursor = connection.execute(stmt)
-
-        # 定义你的LLM
-        llm = Ollama(model="pxlksr/defog_sqlcoder-7b-2:Q8")
-        llm.temperature = 0.2
-        llm.base_url = "http://1.92.64.112:11434"
-
-        # 查询引擎
-        query_engine = NLSQLTableQueryEngine(
-            sql_database=sql_database, tables=[table_name], llm=llm
-        )
+            print(row)
 
         if messages:
             message = messages[-1]
-            response = query_engine.query(message.content + "")
+            self.query_engine.update_prompts('use mysql database')
+            response = self.query_engine.query(message.content + "")
             return ChatMessage(role="assistant", content=response)
         else:
             return ChatMessage(role="assistant", content="can i help you!")

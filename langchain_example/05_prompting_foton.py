4eee7f443ecb3813b9036707fd3e81790ff8d5a5
cai98 <18801086184>
2024-03-27 16:50:56 +0800

    fix

--- langchain_example/05_prompting_foton.py
+++ langchain_example/05_prompting_foton.py
@@ -14,7 +14,6 @@ db = SQLDatabase.from_uri("mysql+pymysql://root:Foton12345&@1.92.64.112/llama", 
 llm = Ollama(model="pxlksr/defog_sqlcoder-7b-2:Q8")
 llm.temperature = 0
 llm.base_url = "http://1.92.64.112:11434"
-chain = create_sql_query_chain(llm, db)
 
 # 从配置表中读取提示信息
 mysql = MysqlUtil(host='1.92.64.112', db='llama_config', user='root', passwd='Foton12345&')
@@ -28,16 +27,7 @@ for res in results:
     examples.append(prompt)
 print(examples)
 
-example_prompt = PromptTemplate.from_template("User input: {input}\nSQL query: {query}")
-prompt = FewShotPromptTemplate(
-    examples=examples,
-    example_prompt=example_prompt,
-    prefix="You are a MySQL expert. Given an input question, create a syntactically correct SQLite query to run. Unless otherwise specificed, do not return more than {top_k} rows.\n\nHere is the relevant table info: {table_info}\n\nBelow are a number of examples of questions and their corresponding SQL queries.",
-    suffix="User input: {input}\nSQL query: ",
-    input_variables=["input", "top_k", "table_info"],
-)
-
-modelPath = 'bge-small-en-v1.5'
+modelPath = '/Users/amalyok/Desktop/foton/llamaindex_learn/langchain_example/bge-small-en-v1.5'
 model_kwargs = {'device':'cpu'}
 encode_kwargs = {'normalize_embeddings': True}
 embeddings = HuggingFaceEmbeddings(
@@ -53,6 +43,16 @@ example_selector = SemanticSimilarityExampleSelector.from_examples(
     k=5,
     input_keys=["input"],
 )
+
+example_prompt = PromptTemplate.from_template("User input: {input}\nSQL query: {query}")
+prompt = FewShotPromptTemplate(
+    example_selector=example_selector,
+    example_prompt=example_prompt,
+    prefix="You are a MySQL expert. Given an input question, create a syntactically correct SQLite query to run. Unless otherwise specificed, do not return more than {top_k} rows.\n\nHere is the relevant table info: {table_info}\n\nBelow are a number of examples of questions and their corresponding SQL queries.",
+    suffix="User input: {input}\nSQL query: ",
+    input_variables=["input", "top_k", "table_info"],
+)
+chain = create_sql_query_chain(llm, db, prompt)
 
 _input = {"question": "终端离线时长小于10的车辆有多少?"}
 ret = chain.invoke(_input)

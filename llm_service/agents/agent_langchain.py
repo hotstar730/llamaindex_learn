c63c1301c8c4b55da9806db10fbf37368b28d835
李欣欣 <lixinxin@rtmap.com>
2024-03-27 16:35:06 +0800

    fix1

--- llm_service/agents/agent_langchain.py
+++ llm_service/agents/agent_langchain.py
@@ -9,7 +9,7 @@
     创建日期： 2024/3/14 13:47
     需要使用mysql数据进行测试
 """
-
+from typing import List
 from langchain.chains.sql_database.query import create_sql_query_chain
 from langchain.globals import set_debug
 from langchain_community.embeddings import HuggingFaceEmbeddings
@@ -18,12 +18,16 @@ from langchain_community.utilities.sql_database import SQLDatabase
 from langchain_community.vectorstores import FAISS
 from langchain_core.example_selectors import SemanticSimilarityExampleSelector
 from langchain_core.prompts import FewShotPromptTemplate, PromptTemplate
+from llama_index.core.base.llms.types import ChatMessage
+
+from llm_service.util.mysql_util import MysqlUtil
 
 
 class AgentLangChainSql:
     _prompt: FewShotPromptTemplate
     _llm: Ollama
     _db: SQLDatabase
+    _example_selector: SemanticSimilarityExampleSelector
 
     def __init__(self) -> None:
         # 1 Dialect-specific prompting
@@ -35,20 +39,24 @@ class AgentLangChainSql:
         self._db = SQLDatabase.from_uri("mysql+pymysql://root:Foton12345&@1.92.64.112/llama", sample_rows_in_table_info=3)
 
         # 3 get prompt
-        self._prompt = self.get_prompt()
+        self._prompt = self._get_prompt()
 
         # get chain
         self.chain = create_sql_query_chain(self._llm, self._db, self._prompt)
 
-    def get_prompt(self) -> FewShotPromptTemplate:
+    def _get_examples(self) -> []:
+        # 从配置表中读取提示信息
+        mysql = MysqlUtil(host='1.92.64.112', db='llama_config', user='root', passwd='Foton12345&')
+        sql = "select * from prompts"
+        results = mysql.get(sql)
+        examples = []
+        for res in results:
+            examples.append({'input': res['input'], 'query': res['query']})
+        return examples
+
+    def _get_prompt(self) -> FewShotPromptTemplate:
         # 3.1 Few-shot examples
-        examples = [
-            {"input": "List all artists.", "query": "SELECT * FROM Artist;"},
-            {
-                "input": "Find all albums for the artist 'AC/DC'.",
-                "query": "SELECT * FROM Album WHERE ArtistId = (SELECT ArtistId FROM Artist WHERE Name = 'AC/DC');",
-            },
-        ]
+        examples = self._get_examples()
 
         # 3.2 Dynamic few-shot examples
         modelPath = '../data/embed_model/bge-small-en-v1.5'
@@ -59,7 +67,7 @@ class AgentLangChainSql:
             model_kwargs=model_kwargs,  # Pass the model configuration options
             encode_kwargs=encode_kwargs  # Pass the encoding options
         )
-        example_selector = SemanticSimilarityExampleSelector.from_examples(
+        self._example_selector = SemanticSimilarityExampleSelector.from_examples(
             examples,
             embeddings,
             FAISS,
@@ -70,7 +78,7 @@ class AgentLangChainSql:
         # 3.3 prompt
         example_prompt = PromptTemplate.from_template("User input: {input}\nSQL query: {query}")
         prompt = FewShotPromptTemplate(
-            example_selector=example_selector,
+            example_selector=self._example_selector,
             example_prompt=example_prompt,
             prefix="You are a MySQL expert. Given an input question, create a syntactically correct SQLite query to run. Unless otherwise specificed, do not return more than {top_k} rows.\n\nHere is the relevant table info: {table_info}\n\nBelow are a number of examples of questions and their corresponding SQL queries.",
             suffix="User input: {input}\nSQL query: ",
@@ -78,22 +86,31 @@ class AgentLangChainSql:
         )
         return prompt
 
-    # def chat(self, messages: List[ChatMessage]) -> ChatMessage:
-    #     if messages:
-    #         message = messages[-1]
-    #         self.query_engine.update_prompts('use mysql database')
-    #         response = self.query_engine.query(message.content + "")
-    #         return ChatMessage(role="assistant", content=response)
-    #     else:
-    #         return ChatMessage(role="assistant", content="can i help you!")
+    def chat(self, messages: List[ChatMessage]) -> ChatMessage:
+        if messages:
+            message = messages[-1]
+            set_debug(True)
+            response = self.chain.invoke({"question": message})
+            return ChatMessage(role="assistant", content=response)
+        else:
+            return ChatMessage(role="assistant", content="can i help you!")
 
-    def chat(self, messages: str) -> str:
+    def chat(self, message: str) -> str:
+        examples = self._example_selector.select_examples({"input": message})
+        response_example = ""
+        for i in examples:
+            response_example += '\n' + i['input']
+        print(examples)
+
         set_debug(True)
-        ret = self.chain.invoke({"question": "how many artists are there?"})
-        print(ret)
-        return ""
+        response = self.chain.invoke({"question": message})
+
+
+
+        return response
 
 
 agent_lang_chain = AgentLangChainSql()
-agent_lang_chain.chat("")
+ret = agent_lang_chain.chat("盘点车辆总数")
+print(ret)
 
